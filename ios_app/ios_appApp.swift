e05ebb24cdf4890a40bffbdcda861002e336c766
Expellishock81637 <due93196@gmail.com>
2024-12-18 23:20:29 +0800

    Initial Commit

---
+++ ios_app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios_app
+//
+//  Created by Williard Liao on 2024/12/18.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
